556d6ff957c90a30d206e36f749b22bd0cb6ed74
HananFitS <114347723+HananFitS@users.noreply.github.com>
2025-01-15 11:58:01 +0700

    update splashscreen.tsx

---
+++ app/SplashScreen.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+      <LottieView
+        source={require('../assets/splash.json')} // Lokasi file JSON Lottie Anda
+        autoPlay
+        loop
+        style={styles.animation}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#492a6f', // Warna latar belakang
+  },
+  animation: {
+    width: 300, // Sesuaikan ukuran
+    height: 300,
+  },
+});
+
+export default SplashScreen;
